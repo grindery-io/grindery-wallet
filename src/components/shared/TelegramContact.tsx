dd077c3a9777f81d9014a41294a2bb662ccd996f
Egor Miliukov <milukove@gmail.com>
2023-10-05 04:58:54 +0300

    fix seelcted contact border

--- src/components/shared/TelegramContact.tsx
+++ src/components/shared/TelegramContact.tsx
@@ -78,7 +78,7 @@ const TelegramContact = ({
           selected
             ? {
                 background: "#E5F4FF",
-                borderColor: "#2AABEE !important",
+                border: "1px solid #2AABEE",
               }
             : {}
         }
