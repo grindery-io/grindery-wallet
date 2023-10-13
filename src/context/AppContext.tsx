f1623a5bcd2517820728865c7855332b3dccd68d
Egor Miliukov <milukove@gmail.com>
2023-10-13 05:25:28 +0300

    fix

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -560,10 +560,6 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
 
   useEffect(() => {
     getPhotos();
-    const controller = abortControllerRef.current;
-    return () => {
-      controller.abort();
-    };
   }, [getPhotos]);
 
   if (window.origin.includes("localhost")) {
