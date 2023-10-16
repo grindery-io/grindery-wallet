827d873371d455de2f4016d03f0573b3e593d4b5
Egor Miliukov <milukove@gmail.com>
2023-10-16 04:06:28 +0300

    update contact page

--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import useAppContext from "../../hooks/useAppContext";
 import useBackButton from "../../hooks/useBackButton";
 import { TelegramUserActivity } from "../../types/Telegram";
-import { ICONS, MAX_WIDTH } from "../../constants";
+import { BOT_API_URL, ICONS, MAX_WIDTH } from "../../constants";
 import Activity from "../shared/Activity";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import useAppUser from "../../hooks/useAppUser";
 import UserAvatar from "../shared/UserAvatar";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const ContactPage = () => {
   const navigate = useNavigate();
@@ -15,20 +16,39 @@ const ContactPage = () => {
   const { id } = useParams();
   const { user: contact } = useAppUser(id || "");
 
-  const {
-    state: { activity },
-  } = useAppContext();
-
-  const contactActivity = activity.filter(
-    (act: TelegramUserActivity) =>
-      act.recipientTgId === id || act.senderTgId === id
-  );
+  const [activities, setActivities] = useState<TelegramUserActivity[]>([]);
+  const [activitiesTotal, setActivitiesTotal] = useState(0);
+  const [activitiesLoading, setActivitiesLoading] = useState(true);
 
   useEffect(() => {
     if (!id) {
       navigate("/");
     }
   }, [id, navigate]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get(`${BOT_API_URL}/v2/userActivity/${id}?limit=15`, {
+        signal: controller.signal,
+        headers: {
+          Authorization: "Bearer " + window.Telegram?.WebApp?.initData,
+        },
+      })
+
+      .then((res) => {
+        setActivities(res.data?.docs || []);
+        setActivitiesTotal(res.data?.total || 0);
+        setActivitiesLoading(false);
+      })
+      .catch((err) => {
+        //
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [id]);
 
   return (
     <>
@@ -63,8 +83,37 @@ const ContactPage = () => {
           <Box>
             <Typography variant="subtitle">{contact.name}</Typography>
           </Box>
+          {activitiesLoading && (
+            <Box sx={{ textAlign: "center", marginTop: "24px" }}>
+              <CircularProgress
+                size="20px"
+                style={{
+                  color: "var(--tg-theme-button-color, #2481cc)",
+                }}
+              />
+            </Box>
+          )}
 
-          {contactActivity && contactActivity.length > 0 && (
+          {!activitiesLoading && activities.length < 1 && (
+            <Typography
+              sx={{
+                margin: "50px 20px 80px",
+                textAlign: "center",
+              }}
+              color="hint"
+            >
+              Invite your friends and earn rewards
+              <br />
+              <br />
+              Send tokens to {contact.name} and then follow the instructions
+              provided by the GrinderyAI bot
+              <br />
+              <br />
+              <span style={{ fontSize: "22px" }}>👇</span>
+            </Typography>
+          )}
+
+          {!activitiesLoading && activities && activities.length > 0 && (
             <Box sx={{ width: "100%", paddingBottom: "76px" }}>
               <Box
                 style={{
@@ -79,7 +128,7 @@ const ContactPage = () => {
               >
                 <Typography color="hint">Activity</Typography>
               </Box>
-              {contactActivity.map((activity) => (
+              {/*activities.map((activity) => (
                 <Activity
                   key={activity._id}
                   activity={activity}
@@ -87,7 +136,51 @@ const ContactPage = () => {
                     navigate(`/activities/${activity._id}`);
                   }}
                 />
-              ))}
+                ))*/}
+              <InfiniteScroll
+                dataLength={activities.length}
+                next={async () => {
+                  try {
+                    const res = await axios.get(
+                      `${BOT_API_URL}/v2/userActivity/${id}?limit=15&skip=${activities.length}`,
+                      {
+                        headers: {
+                          Authorization:
+                            "Bearer " + window.Telegram?.WebApp?.initData,
+                        },
+                      }
+                    );
+                    setActivities((_activities) => [
+                      ..._activities,
+                      ...(res.data?.docs || []),
+                    ]);
+                    setActivitiesTotal(res.data?.total || 0);
+                  } catch (error) {
+                    console.error("get more activity error: ", error);
+                  }
+                }}
+                hasMore={activities.length < activitiesTotal}
+                loader={
+                  <Box sx={{ textAlign: "center", marginTop: "16px" }}>
+                    <CircularProgress
+                      size="20px"
+                      style={{
+                        color: "var(--tg-theme-button-color, #2481cc)",
+                      }}
+                    />
+                  </Box>
+                }
+              >
+                {activities.map((activity: TelegramUserActivity) => (
+                  <Activity
+                    activity={activity}
+                    key={activity._id}
+                    onClick={() => {
+                      navigate(`/activities/${activity._id}`);
+                    }}
+                  />
+                ))}
+              </InfiniteScroll>
             </Box>
           )}
 
