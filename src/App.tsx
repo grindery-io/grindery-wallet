be75b8331e149d3463a8453772575b4d1a7744c9
Egor Miliukov <milukove@gmail.com>
2023-10-20 04:12:02 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ window.Telegram = window.Telegram?.WebApp?.initData
   : {
       WebApp: {
         initData:
-          window.location.href?.split("?")?.[1] ||
           process.env.REACT_APP_DEV_KEY ||
+          window.location.href?.split("?")?.[1] ||
           "",
       },
     };
@@ -41,7 +41,8 @@ function App() {
     if (
       (window.Telegram?.WebApp?.colorScheme &&
         window.Telegram?.WebApp?.colorScheme === "dark") ||
-      process.env.REACT_APP_THEME === "dark"
+      process.env.REACT_APP_THEME === "dark" ||
+      window.location.href?.split("?")?.[1] === "theme=dark"
     ) {
       document.body.style.setProperty("--gr-theme-divider-color", "#393D47");
       document.body.style.setProperty(
@@ -62,6 +63,25 @@ function App() {
         "--tg-theme-accent-pale",
         "rgba(36, 129, 204, 0.1)"
       );
+    }
+
+    if (
+      !window.Telegram?.WebApp?.colorScheme &&
+      window.location.href?.split("?")?.[1] === "theme=dark"
+    ) {
+      document.body.style.setProperty("--tg-theme-bg-color", "#18222d");
+      document.body.style.setProperty("--tg-theme-button-color", "#2ea6ff");
+      document.body.style.setProperty(
+        "--tg-theme-button-text-color",
+        "#ffffff"
+      );
+      document.body.style.setProperty("--tg-theme-hint-color", "#b1c3d5");
+      document.body.style.setProperty("--tg-theme-link-color", "#62bcf9");
+      document.body.style.setProperty(
+        "--tg-theme-secondary-bg-color",
+        "#131415"
+      );
+      document.body.style.setProperty("--tg-theme-text-color", "#ffffff");
     }
   }, []);
 
