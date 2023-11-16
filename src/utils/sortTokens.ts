0785bd92bfaa8ded30cb286e9a322ea54ff71af1
Egor Miliukov <milukove@gmail.com>
2023-11-16 03:57:54 +0200

    fix sortTokens

--- src/utils/sortTokens.ts
+++ src/utils/sortTokens.ts
@@ -4,9 +4,9 @@ import { GRINDERY_ONE_TOKEN } from "../constants";
 const SECOND_TOKEN_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 
 export const sortTokens = (tokens: TokenType[]) => {
-  return tokens.sort((a, b) => {
-    const aAddress = a.address.toLowerCase();
-    const bAddress = b.address.toLowerCase();
+  return [...tokens].sort((a, b) => {
+    const aAddress = a?.address?.toLowerCase() || "";
+    const bAddress = b?.address?.toLowerCase() || "";
 
     if (aAddress === GRINDERY_ONE_TOKEN.address.toLowerCase()) return -1;
     if (bAddress === GRINDERY_ONE_TOKEN.address.toLowerCase()) return 1;
