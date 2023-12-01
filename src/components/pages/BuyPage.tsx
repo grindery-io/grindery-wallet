2967869c5c5ac603247e163e48c15f2b235a107e
Egor Miliukov <milukove@gmail.com>
2023-12-02 01:40:00 +0200

    update

--- src/components/pages/BuyPage.tsx
+++ src/components/pages/BuyPage.tsx
@@ -1,95 +1,73 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { loadMoonPay } from "@moonpay/moonpay-js";
-import useBackButton from "../../hooks/useBackButton";
-import Loading from "../shared/Loading/Loading";
-import { Box } from "@mui/material";
+import React from "react";
+import { MoonPayBuyWidget, MoonPayProvider } from "@moonpay/moonpay-react";
+import useBackButton from "hooks/useBackButton";
 import { selectAppStore, useAppSelector } from "store";
+//import { WALLET_API_URL } from "../../constants";
+import { Box } from "@mui/material";
+import { isDarkTheme } from "utils";
 
 const BuyPage = () => {
   useBackButton();
-  const [loading, setLoading] = useState(true);
   const { user } = useAppSelector(selectAppStore);
 
-  const loadMoonpayWidget = useCallback(async () => {
-    if (!user?.patchwallet || !user?.userTelegramID) {
-      return;
-    }
-    const moonPay = await loadMoonPay();
-
-    const widget = moonPay?.({
-      flow: "buy",
-      environment: "sandbox",
-      variant: "embedded",
-      containerNodeSelector: "#moonpay-container",
-      useWarnBeforeRefresh: false,
-      params: {
-        apiKey: "pk_test_relDpm1G9B54FAcIGvnGFlLLpH5hX",
-        walletAddress: user.patchwallet,
-        colorCode:
-          window.Telegram?.WebApp?.themeParams?.button_color || "#2481cc",
-        theme: window.Telegram?.WebApp?.colorScheme || "light",
-        language: "en",
-        baseCurrencyCode: "usd",
-        externalCustomerId: user.userTelegramID,
-        redirectURL: "/buy/complete",
-      },
-      debug: true,
-    });
-    widget?.show();
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, [user?.patchwallet, user?.userTelegramID]);
-
-  useEffect(() => {
-    loadMoonpayWidget();
-  }, [loadMoonpayWidget]);
+  /*const handleGetSignature = async (url: string): Promise<string> => {
+    const res = await fetch(`${WALLET_API_URL}/v2/buy/sign-url?url=${url}`);
+    const signature = await res.text();
+    return signature;
+  };*/
 
   return (
     <Box
       sx={{
-        position: "relative",
-        flex: 1,
         width: "100%",
-        height: "100vh",
-        maxHeight: "100vh",
+        height: "100%",
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        "& > div": {
+          width: "100%",
+          height: "100vh",
+          flex: 1,
+          margin: "0 !important",
+          padding: "0 16px !important",
+          border: "none !important",
+          borderRadius: "0 !important",
+        },
+        "& iframe": {
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          display: "block",
+          margin: 0,
+          padding: 0,
+        },
       }}
     >
-      {loading && (
-        <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: 999,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "var(--tg-theme-bg-color, #ffffff)",
-          }}
-        >
-          <Loading />
-        </Box>
-      )}
-      <Box
-        sx={{
-          width: "100%",
-          height: "100%",
-          maxHeight: "100vh",
-          "& iframe": {
-            width: "100%",
-            height: "100vh",
-            maxHeight: "100vh",
-            border: "none",
-            padding: 0,
-            margin: 0,
-            display: "block",
-          },
-        }}
-        id="moonpay-container"
-      />
+      <MoonPayProvider
+        apiKey="pk_test_relDpm1G9B54FAcIGvnGFlLLpH5hX"
+        //environment="sandbox"
+        debug
+      >
+        <MoonPayBuyWidget
+          useWarnBeforeRefresh={false}
+          variant="embedded"
+          baseCurrencyCode="usd"
+          baseCurrencyAmount="100"
+          defaultCurrencyCode="eth"
+          onLogin={async () => console.log("Customer logged in!")}
+          visible
+          colorCode={
+            window.Telegram?.WebApp?.themeParams?.button_color || "#2481cc"
+          }
+          theme={isDarkTheme() ? "dark" : "light"}
+          language="en"
+          externalCustomerId={user?.userTelegramID}
+          //walletAddress={user?.patchwallet}
+          //onUrlSignatureRequested={handleGetSignature}
+        />
+      </MoonPayProvider>
     </Box>
   );
 };
